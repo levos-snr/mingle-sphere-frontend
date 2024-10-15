61917739fdf956e7101aa925b77c801256521095
Lewis Odero <lewisodero27@gmail.com>
2024-10-15 22:13:27 +0300

    feat: add flowbite package

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from "flowbite-react";
+
 
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">Starter </h1>
+	   <Button>Click me</Button>
     </>
   )
 }
